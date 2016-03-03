2c520bc2b73fc9073af962f96e5631667242811b
Nathan Hazout <nathanh@il.ibm.com>
2016-03-03 11:35:40 +0200

    Adding licenses

--- src/com/sample/JTVFilter.java
+++ src/com/sample/JTVFilter.java
@@ -1,3 +1,18 @@
+/**
+* Copyright 2016 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.sample;
 
 import java.io.IOException;
@@ -27,7 +42,7 @@ public class JTVFilter implements Filter {
 
 	private TokenValidationManager validator;
 	private FilterConfig filterConfig = null;
-	
+
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		URI uri = null;
@@ -39,7 +54,7 @@ public class JTVFilter implements Filter {
 			System.out.println("Error reading introspection URI");
 		}
 	}
-	
+
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain filter) throws IOException, ServletException {
 		String expectedScope = filterConfig.getInitParameter("scope");
